268ebc60012d2e25098e5fe2a0cd7852824da5b0
Bui-Minh-Tri <bmtril93@gmail.com>
2024-08-23 20:41:54 +0700

    Delete TAY CAM DRONE directory

--- TAY CAM DRONE/ESP_240807_01/TX_UART.ino
+++
@@ -1,7 +0,0 @@
-void TX_UART()
-{
-  for (int i = 0; i < 3; i++)
-  {
-    MegaPort.write((uint8_t*)&sendData[i], sizeof(sendData[i]));
-  }
-}
--- TAY CAM DRONE/ESP_240807_01/convertByteToLong.ino
+++
@@ -1,3 +0,0 @@
-long convertBytesToLong(byte byte1, byte byte2, byte byte3, byte byte4) {
-  return ((long)byte1 << 24) | ((long)byte2 << 16) | ((long)byte3 << 8) | (long)byte4;
-}
